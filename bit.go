ab764db97d53671abfdbbe75edf24c0b695c2077
ki11errabbit <unlikelytitan@gmail.com>
2023-07-01 11:27:51 -0700

    Added documentation

--- bit.go
+++ bit.go
@@ -9,6 +9,9 @@ import (
 	"github.com/jroimartin/gocui"
 )
 
+
+
+
 const BitUp = "▲"
 const BitDown = "▼"
 const BitLeft = "◀"
@@ -16,14 +19,13 @@ const BitRight = "▶"
 
 const HelpText = "n: next step    p: previous step    f: first step    l: last step    s: switch world    q: quit"
 
-const WhiteSquare = "⬜"
-const BlackSquare = "⬛"
-const RedSquare = "🟥"
-const BlueSquare = "🟦"
-const GreenSquare = "🟩"
 
+
+
+/*
+   Enumeration to represent the various colors of a square
+*/
 type Color int
-
 const (
 	White Color = iota
 	Black
@@ -31,7 +33,10 @@ const (
 	Blue
 	Green
 )
-
+/*
+   Deletes the view that shows the steps to replace it with a new one that has been made wide enough
+   to fit the width of the current step.
+*/
 func update_step_view() {
 	gui_i.DeleteView("Steps")
 	if step_v, err := gui_i.SetView("Steps", max_x/2-len(bit_state_actions[current_state]),max_y/2-12,max_x/2+len(bit_state_actions[current_state]), max_y/2-10); err != nil {
@@ -42,7 +47,9 @@ func update_step_view() {
 		fmt.Fprintln(step_v, current_state, ": ", bit_state_actions[current_state])
 	}
 }
-
+/*
+   Goes to the initial step of the bit world
+*/
 func first_state(gui *gocui.Gui, world *gocui.View) error {
 	world.Clear()
 	current_state = 0
@@ -51,6 +58,9 @@ func first_state(gui *gocui.Gui, world *gocui.View) error {
 	return nil
 }
 
+/*
+   Goes to the next step of the bit world.
+*/
 func next_state(gui *gocui.Gui, world *gocui.View) error {
 	if current_state < len(bit_states) - 1 {
 		world.Clear()
@@ -62,6 +72,9 @@ func next_state(gui *gocui.Gui, world *gocui.View) error {
 	return nil
 }
 
+/*
+   Goes to the last step of the bit world.
+*/
 func last_state(gui *gocui.Gui, world *gocui.View) error {
 	world.Clear()
 	current_state = len(bit_states) - 1
@@ -70,6 +83,9 @@ func last_state(gui *gocui.Gui, world *gocui.View) error {
 	return nil
 }
 
+/*
+   Goes to the previous step of the bit world.
+*/
 func prev_state(gui *gocui.Gui, world *gocui.View) error {
 	
 	if current_state > 0 {
@@ -82,8 +98,14 @@ func prev_state(gui *gocui.Gui, world *gocui.View) error {
 	return nil
 }
 
+/*
+   Determines if the we are showing what the final world should look like or what the current step of bit looks like.
+*/
 var final_world bool = false
 
+/*
+   Switches between showing the answer and the current step of bit.
+*/
 func switch_world(gui *gocui.Gui, world *gocui.View) error {
 	world.Clear()
 	final_world = !final_world
@@ -95,21 +117,31 @@ func switch_world(gui *gocui.Gui, world *gocui.View) error {
 	return nil
 }
 
+/*
+   Function that quits the bit gui.
+*/
 func quit(gui *gocui.Gui, view *gocui.View) error {
 	return gocui.ErrQuit
 }
 
+/*
+   This function resets the global variable that are used to keep track of the gui.
+*/
 func reset_gui_globals() {
 	gui_i = nil
 	max_x = 0
 	max_y = 0
 }
-
+// This is a pointer to the gui that is being used to display the bit world.
 var gui_i *gocui.Gui = nil
+//This represents the width of the gui.
 var max_x int = 0
+//This represents the height of the gui.
 var max_y int = 0
 
-
+/*
+   This function draws the bit world in the gui and sets the keybindings for it.
+*/
 func setup_world_view(gui *gocui.Gui, x0 int, y0 int, x1 int, y1 int) error {
 	
 	if world, err := gui.SetView("World", x0, y0, x1, y1); err != nil {
@@ -148,6 +180,9 @@ func setup_world_view(gui *gocui.Gui, x0 int, y0 int, x1 int, y1 int) error {
 	return nil
 }
 
+/*
+   This function initializes the step view for bit.
+*/
 func setup_step_view(gui *gocui.Gui, x0 int, y0 int, x1 int, y1 int) error {
 	if step_view, err := gui.SetView("Steps", x0, y0, x1, y1); err != nil {
 		if err != gocui.ErrUnknownView {
@@ -158,6 +193,9 @@ func setup_step_view(gui *gocui.Gui, x0 int, y0 int, x1 int, y1 int) error {
 	return nil
 }
 
+/*
+   This function initializes the help view for the bit gui.
+*/
 func setup_help_view(gui *gocui.Gui, x0 int, y0 int, x1 int, y1 int) error {
 	
 	if help_view, err := gui.SetView("Help", x0, y0, x1, y1); err != nil {
@@ -169,6 +207,10 @@ func setup_help_view(gui *gocui.Gui, x0 int, y0 int, x1 int, y1 int) error {
 	return nil
 }
 
+/*
+   This function launches the gui for bit.
+   
+*/
 func RunGui() {
 	gui, err := gocui.NewGui(gocui.OutputNormal)
 	if err != nil {
@@ -231,6 +273,9 @@ func RunGui() {
 
 }
 
+/*
+   This function resets the global variables that bit and the gui use to keep track of the state.
+*/
 func reset_bit_globals() {
 	bit_snapshots = make([]int, 0)
 	current_snapshot = 0
@@ -242,19 +287,31 @@ func reset_bit_globals() {
 	world_height = 0
 }
 
+// Snapshots of the world at steps the user has created.
 var bit_snapshots []int = make([]int, 0)
+var bit_snapshot_names []string = make([]string, 0)
 
+// The current snapshot of the world.
 var current_snapshot int = 0
 
+// The current state of the world, used by the gui.
 var current_state int = 0
+// The states of the world at each step that bit has taken.
 var bit_states []*Bit = make([]*Bit, 1)
+// A list that is the action that bit made at a step
 var bit_state_actions []string = make([]string, 1)
 
+// A global that specifies that an error has occured so that we can skip every other bit step.
 var error_occured error = nil
 
+// The width of the world.
 var world_width int = 0
+// The height of the world.
 var world_height int = 0
 
+/*
+   This function initializes the bit globals.
+*/
 func init_bit_state(bit *Bit) {
 	dest := make([][]Square, len(bit.world))
 	for i := range dest {
@@ -265,6 +322,10 @@ func init_bit_state(bit *Bit) {
 	bit_state_actions[0] = "initial state"
 }
 
+/*
+   This function is called whenever bit does an action.
+   This adds the action to the list of actions that bit has taken.
+*/
 func add_bit_state(bit *Bit, action string) {
 	dest := make([][]Square, len(bit.world))
 	for i := range dest {
@@ -275,22 +336,40 @@ func add_bit_state(bit *Bit, action string) {
 	bit_state_actions = append(bit_state_actions, action)
 }
 
+/*
+   This function is called whenever bit makes an invalid move.
+   This function adds the error to the list of actions that bit has taken.
+*/
 func stop_display_error(msg string, bit *Bit) {
 	error_occured = errors.New(msg)
 	add_bit_state(bit, msg)
 }
 
+/*
+   This Struct represents a square in the world.
+   It has a color and a boolean that represents whether or not it has a bit.
+*/
 type Square struct {
 	color Color
 	has_bit bool
 }
 
-var print_pair bool = false
+/*
+   This function loads the world from a file.
+   It returns a 2d array of squares.
+   The format of the file is as follows:
+   Use 'u' 'd' 'l' 'r' to represent the direction that the bit will start facing.
+   then use '-' to represent a white square, 'r' to represent a red square, 'b' to represent a blue square, 'g' to represent a green square, and 'x' to represent a black square.
+   Then specify the starting position of the bit with coordinates x y.
 
-func set_print_pair() {
-	print_pair = true
-}
-
+Example:
+   u
+   -r-r-
+   -b-b-
+   -r-r-
+   0 0
+   
+*/
 func load_world(file_name string) [][]Square {
 	file, err := os.Open(file_name)
 	if err != nil {
@@ -338,6 +417,13 @@ func load_world(file_name string) [][]Square {
 	return world
 }
 
+
+/*
+   This function returns a bit and takes in the starting world and the ending world.
+   The starting world is the world that bit starts in.
+   The ending world is the world that bit must reach.
+   This function should be called to get a bit.
+*/
 func GetBit(start_world string, end_world string) *Bit {
 	reset_bit_globals()
 	file, err := os.Open(start_world)
@@ -371,7 +457,10 @@ func GetBit(start_world string, end_world string) *Bit {
 	init_bit_state(bit)
 	return bit
 }
-	
+
+/*
+   Helper function for locating bit for the initial world.
+*/
 func find_bit_x(world [][]Square) int {
 	for _, row := range world {
 		for j, square := range row {
@@ -383,6 +472,9 @@ func find_bit_x(world [][]Square) int {
 	return 0
 }
 
+/*
+   Helper function for locating bit for the initial world.
+*/
 func find_bit_y(world [][]Square) int {
 	for i, row := range world {
 		for _, square := range row {
@@ -395,7 +487,14 @@ func find_bit_y(world [][]Square) int {
 }
 		
 	
-
+/*
+   This struct represents a bit.
+   It has a face that represents the direction that it is facing.
+   It has a number of steps that represents the number of steps it has taken.
+   It has an x and y coordinate that represents its position in the world.
+   It has a world that represents the world that it is currently in.
+   It has a final_world that represents the world that it must reach.
+*/
 type Bit struct {
 	face string
 	steps int
@@ -405,6 +504,10 @@ type Bit struct {
 	final_world [][]Square
 }
 
+/*
+   This creates a new bit pointer.
+   This function should not be called directly.
+*/
 func new_bit(direction string, world [][]Square, final_world[][]Square) *Bit {
 	return &Bit{
 		face: direction,
@@ -416,6 +519,11 @@ func new_bit(direction string, world [][]Square, final_world[][]Square) *Bit {
 	}
 }
 
+
+/*
+   This function prints a bit to the screen.
+   It takes in a view and a bit face and a world.
+*/ 
 func print_world(v *gocui.View, face string, world [][]Square) {
 	for _, row := range world {
 			for i := 0; i < 3; i++ {
@@ -456,6 +564,10 @@ func print_world(v *gocui.View, face string, world [][]Square) {
 	}
 }
 
+/*
+   This function moves bit in the direction that it is facing.
+   It will mark an error if bit tries to move out of bounds or onto a black square.
+*/
 func (bit *Bit) Move() {
 	if error_occured != nil {
 		return
@@ -479,6 +591,10 @@ func (bit *Bit) Move() {
 	add_bit_state(bit, "move     ")
 }
 
+/*
+   Internal function for moving bit left.
+   Returns an error if bit tries to make an invalid move.
+*/
 func (b *Bit) moveUp() error {
 	if b.y -1 < 0 {
 		return errors.New("Out of bounds")
@@ -493,6 +609,10 @@ func (b *Bit) moveUp() error {
 	return nil
 }
 
+/*
+   Internal function for moving bit down.
+   Returns an error if bit tries to make an invalid move.
+*/
 func (b *Bit) moveDown() error {
 	if b.y +1 > len(b.world) {
 		return errors.New("Out of bounds")
@@ -507,6 +627,10 @@ func (b *Bit) moveDown() error {
 	return nil
 }
 
+/*
+   Internal function for moving bit left.
+   Returns an error if bit tries to make an invalid move.
+*/
 func (b *Bit) moveLeft() error {
 	if b.x -1 < 0 {
 		return errors.New("Out of bounds")
@@ -521,6 +645,10 @@ func (b *Bit) moveLeft() error {
 	return nil
 }
 
+/*
+   Internal function for moving bit right.
+   Returns an error if bit tries to make an invalid move.
+*/
 func (b *Bit) moveRight() error {
 	if b.x +1 > len(b.world[0]) -1 {
 		return errors.New("Out of bounds")
@@ -535,6 +663,9 @@ func (b *Bit) moveRight() error {
 	return nil
 }
 
+/*
+   This function makes bit turn left without moving.
+*/
 func (b *Bit) Left() {
 	if error_occured != nil {
 		return
@@ -556,6 +687,9 @@ func (b *Bit) Left() {
 	pause()*/
 }
 
+/*
+   This function makes bit turn right without moving.
+*/
 func (b *Bit) Right() {
 	if error_occured != nil {
 		return
@@ -574,22 +708,37 @@ func (b *Bit) Right() {
 	add_bit_state(b, "right       ")
 }
 
+/*
+   This function causes bit to paint a color on the current square with a given color.
+   Valid colors are "red", "blue", and "green".
+*/
 func (bit *Bit) Paint(color string) {
 	if error_occured != nil {
 		return
 	}
+	var success bool = false
 	if color == "red" {
 		bit.world[bit.y][bit.x].color = Red
+		success = true
 	}
 	if color == "blue" {
 		bit.world[bit.y][bit.x].color = Blue
+		success = true
 	}
 	if color == "green" {
 		bit.world[bit.y][bit.x].color = Green
+		success = true
+	} else {
+		stop_display_error("Invalid color: " + color,bit)
 	}
-	add_bit_state(bit, "paint " + color)
+	if success {
+		add_bit_state(bit, "paint " + color)
+	}
 }
 
+/*
+   This function causes bit to erase the color on the current square.
+*/
 func (bit *Bit) Erase() {
 	if error_occured != nil {
 		return
@@ -598,6 +747,10 @@ func (bit *Bit) Erase() {
 	add_bit_state(bit, "erase")
 }
 
+/*
+   This function returns the color of the current square.
+   Valid colors are "red", "blue", "green", and "white".
+*/
 func (bit *Bit) GetColor() string {
 	if error_occured != nil {
 		return ""
@@ -615,6 +768,9 @@ func (bit *Bit) GetColor() string {
 	}
 }
 
+/*
+   This function checks if the the current square is red.
+*/
 func (bit *Bit) IsRed() bool {
 	if error_occured != nil {
 		return false
@@ -623,7 +779,10 @@ func (bit *Bit) IsRed() bool {
 	return bit.world[bit.y][bit.x].color == Red
 }
 
-func (bit *Bit) IsRlue() bool {
+/*
+   This function checks if the the current square is blue.
+*/
+func (bit *Bit) IsBlue() bool {
 	if error_occured != nil {
 		return false
 	}
@@ -631,6 +790,9 @@ func (bit *Bit) IsRlue() bool {
 	return bit.world[bit.y][bit.x].color == Blue
 }
 
+/*
+   This function checks if the the current square is green.
+*/
 func (bit *Bit) IsGreen() bool {
 	if error_occured != nil {
 		return false
@@ -639,6 +801,10 @@ func (bit *Bit) IsGreen() bool {
 	return bit.world[bit.y][bit.x].color == Green
 }
 
+/*
+   This function checks if the square infront of bit is clear.
+   Meaning that it is not black and is within the bounds of the world.
+*/
 func (bit *Bit) IsFrontClear() bool {
 	if error_occured != nil {
 		return false
@@ -657,6 +823,10 @@ func (bit *Bit) IsFrontClear() bool {
 	return false
 }
 
+/*
+   This function checks if the square to the right of bit is clear.
+   Meaning that it is not black and is within the bounds of the world.
+*/
 func (bit *Bit) IsRightClear() bool {
 	if error_occured != nil {
 		return false
@@ -675,6 +845,10 @@ func (bit *Bit) IsRightClear() bool {
 	return false
 }
 
+/*
+   This function checks if the square to the left of bit is clear.
+   Meaning that it is not black and is within the bounds of the world.
+*/
 func (bit *Bit) IsLeftClear() bool {
 	if error_occured != nil {
 		return false
@@ -693,16 +867,22 @@ func (bit *Bit) IsLeftClear() bool {
 	return false
 }
 
-
-func (bit *Bit) Snapshot() {
+/*
+   This function creates a new snapshot of the current state of the world.
+   It takes a string as a parameter which is the name of the snapshot.
+*/
+func (bit *Bit) Snapshot(name string) {
 	if error_occured != nil {
 		return
 	}
-	add_bit_state(bit, "snapshot       ")
+	add_bit_state(bit, "snapshot " + name)
 	bit_snapshots = append(bit_snapshots, bit.steps)
+	bit_snapshot_names = append(bit_snapshot_names, name)
 }
 
-
+/*
+   This function checks to see if the current state of the world matches the final state of the world.
+*/
 func (bit *Bit) Compare() {
 	var matches bool = true
 
